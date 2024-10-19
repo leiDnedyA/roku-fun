539dcfba2b0db465699b658779bbd332134541b4
ayden <aydendiel@gmail.com>
2024-10-19 01:56:02 -0400

    feat: added function to open attack on titan

--- main.py
+++ main.py
@@ -1,18 +1,11 @@
 import argparse
 import requests
+from util.attack_on_titan import launch_attach_on_titan
 from util.roku_connection import scan_roku_ip, test_connection
-from util.roku_commands import keypress
+from util.roku_commands import keypress, launch_disney_plus
 
-key_names = ['home', 'rev', 'fwd', 'play', 'select', 'left', 'right', 'down', 'up', 'back', 'instantreplay', 'info', 'backspace', 'search', 'enter']
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description="A script for doing stuff with roku TV")
-    roku_ip = scan_roku_ip()
-    connection_successful = test_connection(roku_ip)
-
-    if not connection_successful:
-        print(f"ERROR: connection test failed. Attempted to connect to IP: \"{roku_ip}\"")
-
+def run_remote():
+    key_names = ['home', 'rev', 'fwd', 'play', 'select', 'left', 'right', 'down', 'up', 'back', 'instantreplay', 'info', 'backspace', 'search', 'enter']
     while True:
         next_command = input("Enter a command, or 'help' for a list of commands:")
         if next_command.lower() in key_names:
@@ -20,7 +13,18 @@ if __name__ == '__main__':
         else:
             print(f"Valid commands: {','.join(key_names)}")
 
-    keypress_home_url = f'{roku_ip}keypress/home'
-    print(test_connection(roku_ip))
-    r = requests.post(keypress_home_url, data={})
-    print(r)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="A script for doing stuff with roku TV")
+    parser.add_argument('--as-remote', dest='as_remote', action='store_true', help='Run the script as a remote')
+    args = parser.parse_args()
+
+    roku_ip = scan_roku_ip()
+    connection_successful = test_connection(roku_ip)
+
+    if not connection_successful:
+        print(f"ERROR: connection test failed. Attempted to connect to IP: \"{roku_ip}\"")
+
+    if args.as_remote:
+        run_remote()
+    else:
+        launch_attach_on_titan(roku_ip)
--- util/roku_commands.py
+++ util/roku_commands.py
@@ -1,6 +1,12 @@
 import requests
 
+DISNEY_PLUS_CHANNEL_ID = "291097"
+
 def keypress(roku_ip: str, key: str) -> bool:
     command_url = f'{roku_ip}keypress/{key}'
     r = requests.post(command_url)
+    return r.ok
+
+def launch_disney_plus(roku_ip: str) -> bool:
+    r = requests.post(f'{roku_ip}launch/{DISNEY_PLUS_CHANNEL_ID}')
     return r.ok
